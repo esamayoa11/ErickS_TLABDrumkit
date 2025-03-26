d59a7fb9fe92326b98acf4eee3d91e10632f4b49
Erick Samayoa <ericksamayoa11@gmail.com>
2025-03-26 00:36:15 -0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.drum-key[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
 
     audio.currentTime = 0;
