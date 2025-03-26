3b9ea98062483a80086a933dfc2783dfbeeb26b4
Erick Samayoa <ericksamayoa11@gmail.com>
2025-03-26 15:31:47 -0700

    removed extra div preventing code from displaying webpage correctly.

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function playSound(e) {
 }
 
 function removeTransition(e) {
-    if (e.propertyname !== "transform") return;
+    if (e.propertyName !== "transform") return;
     this.classList.remove("playing");
 }
 
-const keys = document.querySelectorAll(".key");
+const keys = d  ocument.querySelectorAll(".key");
 keys.forEach(key => key.addEventListener ("transitionend", removeTransition));
 window.addEventListener("keydown", playSound);
